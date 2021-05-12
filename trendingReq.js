c6e8f10ca239c1e89ecdc83833ef52b7bcb26525
diegopulido2dE <diegopulido2d@gmail.com>
2021-05-12 00:09:45 -0500

    Commit 2

--- trendingReq.js
+++ trendingReq.js
@@ -13,16 +13,22 @@
 
             const trendingSliderCont = document.getElementsByClassName('trendingSliderCont')[0];
             const trendingGifoCard = document.createElement("div");
+            trendingGifoCard.classList.add("trendingGifoCard");
 
             const gifoContainer = document.createElement("div");
+            gifoContainer.classList.add("gifoContainer");
             const gifo = document.createElement("img");
+            gifo.classList.add("gifo");
             gifo.src = result.data[i].url;
             gifoContainer.appendChild(gifo);
 
             const gifoOverlay = document.createElement("div");
+            gifoOverlay.classList.add("gifoOverlay");
             const gifoUser = document.createElement("h5");
+            gifoUser.classList.add("gifoUser");
             gifoUser.innerHTML= result.data[i].username;
             const gifoTitle = document.createElement("h4");
+            gifoTitle.classList.add("gifoTitle");
             gifoTitle.innerHTML= result.data[i].title;
             gifoOverlay.appendChild(gifoUser);
             gifoOverlay.appendChild(gifoTitle);
