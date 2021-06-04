5cf09c051967175558f564adccfe51644ee577a0
Diego Pulido <diegopulido2d@gmail.com>
2021-06-04 17:40:21 -0500

    asdzxc

--- apiReq.js
+++ apiReq.js
@@ -1,13 +1,13 @@
     //TRENDING TOPICS
 
-    const apiKey= 'DvXspQNW16DQZwZ7HNMM2CUYkkHBiXHO'
+    const apiKey= 'DvXspQNW16DQZwZ7HNMM2CUYkkHBiXHO';
     const trendingTopicsReq = 'https://api.giphy.com/v1/trending/searches?api_key='+apiKey; 
+
 
     fetch(trendingTopicsReq)
     .then(response => response.json())
     .then(result => {
-        for (i = 0; i < 5; i++){
-            
+        for (i = 0; i < 4; i++){
 
             var trendingTopicsSec = document.getElementsByClassName('trendingTopics')[0];
             var trendingTopic = document.createElement("span");
@@ -18,9 +18,11 @@
                 trendingTopic.innerHTML = result.data[i];
             } else{
                 trendingTopic.innerHTML = result.data[i]+", ";
-            }
-            
+            }            
+
         }
     })
     .catch(error => console.log('error', error));
+    
+
     
---
+++ autocomplete.js
@@ -0,0 +1,55 @@
+    //AUTOCOMPLETE
+
+    const apiKey8= 'DvXspQNW16DQZwZ7HNMM2CUYkkHBiXHO';
+    const searchInput = document.getElementById('search');
+    const searchWrapper = document.querySelector('.wrapper');
+    const resultsWrapper = document.querySelector('.results');
+
+
+    function autocomp() {
+    
+            let busq = search.value;
+            const autocompReq = 'https://api.giphy.com/v1/gifs/search/tags?api_key='+apiKey8+'&q='+busq; 
+    
+            fetch(autocompReq)
+            .then(response => response.json())
+            .then(result => {
+
+                let resTags = [];
+                let input = searchInput.value;
+                
+                var resultNames = Object.keys(result.data);
+                var resultLength = resultNames.length;
+                
+                for (i = 0; i < resultLength; i++){
+                     let resTag = result.data[i].name;
+                     console.log(resTag);
+                     resTags.push(resTag);
+                }       
+                
+                if (input.length) {
+                    renderResults(resTags);
+                }
+                
+            })
+            .catch(error => console.log('error', error));
+
+    }
+    
+    
+    
+    function renderResults(results) {
+        if (!results.length) {
+        return searchWrapper.classList.remove('show');
+        }
+    
+        const content = results
+        .map((item) => {
+            return `<li>${item}</li>`;
+        })
+        .join('');
+    
+        searchWrapper.classList.add('show');
+        resultsWrapper.innerHTML = `<ul>${content}</ul>`;
+    }
+    
